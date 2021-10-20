a08e1c36baa6014cc0e44912a54808122400a942
danielvonboros <dboros40@gmail.com>
2021-10-20 17:42:49 +0200

    implement filter functionality

--- src/components/list-view/list-view.jsx
+++ src/components/list-view/list-view.jsx
@@ -2,7 +2,6 @@ import { ListItem } from "../list-item/list-item";
 import "./list-view.css"
 
 export function ListView(props) {
-    
     console.log(props.graphs);
 
     if (props.graphs === undefined) {
@@ -11,15 +10,15 @@ export function ListView(props) {
 
     return( 
         <div className="list-container">
-            {props.graphs.filter((graph) => {
-                if(props.searchInput !== '') {const newGraphs = props.graphs.name.toLowerCase().includes(props.searchInput.toLowerCase())
-                return newGraphs} else {return graph}
-            }).map(graph => {
+            {props.graphs.filter( function (graphs) {
+                if (props.searchInput !== '') return graphs.name.toLowerCase().includes(props.searchInput.toLowerCase())
+                 else {return graphs}
+            }).map(graphs => {
                 return (
-                        <div key={`gco_${graph.id}`}  className="graph-container-outer">
-                        <h2 key={`h2_${graph.id}`} className="heading2-light">{graph.name}</h2>
-                        <div id={`c+${graph.id}`} key={`gc_${graph.id}`} className="graph-container"></div>
-                            <ListItem className="list-item" key={graph.id} graph={graph} id={graph.id} />
+                        <div key={`gco_${graphs.id}`}  className="graph-container-outer">
+                        <h2 key={`h2_${graphs.id}`} className="heading2-light">{graphs.name}</h2>
+                        <div id={`c+${graphs.id}`} key={`gc_${graphs.id}`} className="graph-container"></div>
+                            <ListItem className="list-item" key={graphs.id} graph={graphs} id={graphs.id} />
                         </div>
                 )
         })}
